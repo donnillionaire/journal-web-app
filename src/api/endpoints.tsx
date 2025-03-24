227470aed2de530daedb18066ae71bf87fd4166a
donnillionaire <donaldodhiambo09@gmail.com>
2025-03-24 17:56:55 +0300

    feat: endpoints declarations file added

---
+++ src/api/endpoints.tsx
@@ -0,0 +1,31 @@
+const BASE_URL = "http://localhost:8080";
+const journalsURL = "/api/journals";
+const authURL = "/auth/user";
+
+interface AuthAPI {
+  registerUser: string;
+  login: string;
+  getUser: string;
+}
+
+interface JournalAPI {
+  createJournal: string;
+  getJournals: string;
+  getJournalByID: (id: string | number) => string;
+}
+
+const authAPI: AuthAPI = {
+  registerUser: `${BASE_URL}${authURL}/register`,
+  login: `${BASE_URL}${authURL}/login`,
+  getUser: `${BASE_URL}${authURL}/profile`,
+};
+
+const journalAPI: JournalAPI = {
+  createJournal: `${BASE_URL}${journalsURL}`,
+  getJournals: `${BASE_URL}${journalsURL}`,
+  getJournalByID: (id) => `${BASE_URL}${journalsURL}/${id}`, // ID now type-checked
+};
+
+const API = { authAPI, journalAPI };
+
+export default API;
